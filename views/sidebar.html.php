db846e2c395a0913c30095af1c075982a8744566
prasunsen <prasunsen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-05 15:57:31 +0000

    upload missing sidebar file
    git-svn-id: https://plugins.svn.wordpress.org/hostel/trunk@1020336 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/sidebar.html.php
@@ -0,0 +1,57 @@
+<style type="text/css">
+.wphostel-sidebox
+{
+	border:1px solid black;
+	padding:10px;
+	margin-bottom: 20px;
+}
+
+#wphsotel-sidebar
+{
+	float:left;
+	width:25%;
+}
+
+#wphostel-related {
+	background-color: #BBEEDD;
+}
+</style>
+
+<div id="wphostel-related" class="wphostel-sidebox">
+	<h3><?php _e('Go PRO!', 'wphostel')?></h3>
+	
+	<p><?php _e('Get more powerful features by', 'wphostel')?> <a href="http://wp-hostel.com" target="_blank"><?php _e('upgrading to the PRO version:', 'wphostel')?></a></p>
+	
+	<ol>
+		<li><strong><?php _e('Premium support', 'wphostel')?></strong>. <?php _e('Free email support for 1 year after purchase', 'wphostel')?></li>
+		<li><strong><?php _e('Advanced shortcodes', 'wphostel')?></strong></li> 
+		<li><strong><?php _e('Booking availability calendars', 'wphostel')?></strong></li>
+		<li><strong><?php _e('Discounts and surcharges', 'wphostel')?></strong></li>
+		<li><strong><?php _e('Manage addon services', 'wphostel')?></strong></li>
+		<li><strong><?php _e('Reports and charts', 'wphostel')?></strong></li>
+		<li><strong><?php _e('Customizable form code', 'wphostel')?></strong></li>
+		<li><?php _e('And a lot more - just come to see!', 'wphostel')?></li>
+	</ol>
+	
+	<p><?php _e('Just go ahead and', 'wphostel')?> <a href="http://wp-hostel.com/tour.php" target="_blank"><?php _e('take the tour', 'wphostel')?></a> <?php _e('or check the', 'wphostel')?> <a href="http://wp-hostel.com/demo.php" target="_blank"><?php _e('online demo.', 'wphostel')?></a></p>
+	
+	<p><?php _e('Upgrading to PRO comes with 60 days refund policy, one year free upgrades and one year free support.', 'wphostel')?></p>
+</div>
+
+<div id="bft-ad" class="wphostel-sidebox">
+	<h3><?php _e('Help To Spread The Word!', 'wphostel')?></h3>
+	
+	<p><?php _e('If you like this plugin you can do some of the following:', 'wphostel')?></p>
+	
+	<ol>
+		<li><a href="http://wordpress.org/extend/plugins/hostel/" target="_blank"><?php _e('Rate it 5 starts at Wordpress.org', 'wphostel')?></a></li>
+		<li><?php _e('Let others know about it by blogging or posting', 'wphostel')?> <a href="http://wp-hostel.com" target="_blank"><?php _e('our home page', 'wphostel')?></a> <?php _e('on social networks.', 'wphostel')?></li>
+		<li><a href="http://wp-hostel.com" target="_blank"><?php _e('Go PRO', 'wphostel')?></a> <?php _e('to empower your site with extra benefits', 'wphostel')?></li>
+	</ol>
+</div>
+
+<div id="bft-help" class="wphostel-sidebox">
+	<h3><?php _e('Need Support?', 'wphostel')?></h3>
+	
+	<p><?php _e('Please ask your question in the', 'wphostel')?> <a href="http://wordpress.org/support/plugin/hostel" target="_blank"><?php _e('support forum at WordPress.', 'http://wordpress.org/support/plugin/hostel')?></a></p>	
+</div>
